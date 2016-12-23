0eb0c83c9a64d860131343934656c21693546dc2
jchemma <chemmalakuzhyj@gmail.com>
2016-12-22 19:08:51 -0500

    changes to Ui, removed root layer in favor of presenting the main stage; created item class for model of future items that will be created

--- src/inventorymanager/InventoryManager.java
+++ src/inventorymanager/InventoryManager.java
@@ -1,48 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inventorymanager;
 
-import inventorymanager.util.Item;
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author jchemma
- */
-public class InventoryManager extends Application {
+public class InventoryManager extends Application{
     
     private Stage primaryStage;
     private BorderPane rootLayout;
-    private ObservableList<Item> itemType = FXCollections.observableArrayList();
-    
-    public InventoryManager(){
-        //add item types here
-    }
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage){
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Inventory Manager");
-        initRootLayout();
-        showInventoryManager();
+        
+        initInventoryManager();
     }
     
-    public void initRootLayout(){
-        
+    public void initInventoryManager(){
         try{
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(InventoryManager.class.getResource("view/RootLayout.fxml"));
+            loader.setLocation(InventoryManager.class.getResource("view/MainPage.fxml"));
             rootLayout = (BorderPane) loader.load();
             
             Scene scene = new Scene(rootLayout);
@@ -50,27 +31,14 @@ public class InventoryManager extends Application {
             primaryStage.show();
         }catch(IOException ioe){
             ioe.printStackTrace();
-        }        
-    }
-    
-    public void showInventoryManager(){
-        try{
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(InventoryManager.class.getResource("view/InventoryManager.fxml"));
-            AnchorPane inventoryManagerPane = loader.load();
-            
-            rootLayout.setCenter(inventoryManagerPane);
-            
-        }catch(IOException ioe){
-            ioe.printStackTrace();
         }
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
+    
+    public Stage getPrimaryStage(){
+        return primaryStage;
     }
     
+    public static void main(String[]args){
+        launch(args);
+    }
 }
--- src/inventorymanager/util/Item.java
+++ src/inventorymanager/util/Item.java
@@ -1,5 +1,75 @@
 package inventorymanager.util;
 
-public enum Item{
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Item{
     
+    private final StringProperty name;
+    private final StringProperty location;
+    private final DoubleProperty cost;
+    private final IntegerProperty size;
+    
+    public Item(){
+        this(null,null,null,null);
+    }
+    
+    public Item(String name, String location, Double cost, Integer size){
+        this.name = new SimpleStringProperty(name);
+        this.location = new SimpleStringProperty(location);
+        this.cost = new SimpleDoubleProperty(cost);
+        this.size = new SimpleIntegerProperty(size);
+    }
+    
+    public String getName(){
+        return name.get();
+    }
+    
+    public void setName(String newName){
+        name.set(newName);
+    }
+    
+    public StringProperty getNameProperty(){
+        return name;
+    }
+    
+    public String getLocation(){
+        return location.get();
+    }
+    
+    public void setLocation(String newLocation){
+        location.set(newLocation);
+    }
+    
+    public StringProperty getLocationProperty(){
+        return location;
+    }
+    
+    public double getCost(){
+        return cost.get();
+    }
+    
+    public void setCost(double newCost){
+        cost.set(newCost);
+    }
+    
+    public DoubleProperty getCostProperty(){
+        return cost;
+    }
+    
+    public int getSize(){
+        return size.get();
+    }
+    
+    public void setSize(int newSize){
+        size.set(newSize);
+    }
+    
+    public IntegerProperty getIntegerProperty(){
+        return size;
+    }
 }
